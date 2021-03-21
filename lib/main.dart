fda61beb153b6ec8017f3bf2e8000a01d2bd4f89
ishandeveloper <ishandeveloper@outlook.com>
2021-03-21 22:52:55 +0530

    Fixed Font

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class CodeRedApp extends StatelessWidget {
               TargetPlatform.android: CupertinoPageTransitionsBuilder()
             }),
             textTheme: Theme.of(context).textTheme.apply(
-                fontFamily: 'Product Sans', displayColor: Color(0xff2A2A2A))),
+                fontFamily: 'ProductSans', displayColor: Color(0xff2A2A2A))),
         home: StreamBuilder<User>(
             stream: FirebaseAuth.instance.authStateChanges(),
             builder: (context, snapshot) {
